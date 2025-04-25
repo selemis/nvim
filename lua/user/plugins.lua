71f1f1066c543fbbe6463ffa10410689db75f9c8
Stelios Omirou <selemis@gmail.com>
2025-04-26 01:59:43 +0300

    replace dragvisuals plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -19,7 +19,37 @@ return {
                 only_in_normal_buffers = true
             })
         end
-    }
+    },
+
+    -- Replacement for dragvisuals.vim
+    {
+        'echasnovski/mini.move',
+        version = false,
+        event = "VeryLazy",
+        config = function()
+            require('mini.move').setup({
+                -- Module mappings. Use `''` (empty string) to disable one.
+                mappings = {
+                    -- Move visual selection in Visual mode
+                    left = '<LEFT>',
+                    right = '<RIGHT>',
+                    down = '<DOWN>',
+                    up = '<RIGHT>',
+
+                    -- Move current line in Normal mode
+                    line_left = '',
+                    line_right = '',
+                    line_down = '',
+                    line_up = '',
+                },
+                -- Options which control moving behavior
+                options = {
+                    -- Automatically reindent selection during linewise vertical move
+                    reindent_linewise = false,
+                },
+            })
+        end
+    },
 
   -- Add more plugins here as we migrate them
 }
